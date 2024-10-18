26332ef2bf31c6985835571358d246626d4bec95
Wellyson Lopes <wellyson.dev@gmail.com>
2024-10-18 19:16:03 -0300

    Cria autenticação para login criar e editar cadastro para o administrador

---
+++ app/controllers/admins/home_controller.rb
@@ -0,0 +1,6 @@
+module Admins
+  class HomeController < Admins::ApplicationAdminController
+    def index
+    end
+  end
+end
---
+++ app/controllers/admins/registrations_controller.rb
@@ -0,0 +1,38 @@
+module Admins
+  class RegistrationsController < Devise::SessionsController
+    def new
+      @admin = Admin.new
+    end
+
+    def create
+      @admin = Admin.new(admin_params)
+      if @admin.save
+        sign_in(@admin)
+        redirect_to root_path, notice: "Admin created successfully."
+      else
+        render :new
+      end
+    end
+
+    def edit
+      @admin = current_admin
+    end
+
+
+    def update
+      @admin = current_admin
+      if @admin.update(admin_params)
+        sign_in(@admin)
+        redirect_to root_path, notice: "Admin updated successfully."
+      else
+        render :edit
+      end
+    end
+
+    private
+
+    def admin_params
+      params.require(:admin).permit(:name, :cpf, :phone, :email, :password, :admin, :public_servant, :password_confirmation)
+    end
+  end
+end
