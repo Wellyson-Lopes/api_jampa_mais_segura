3b739344cac1e42f10955db339ac10611011707b
Wellyson Lopes <wellyson.dev@gmail.com>
2024-10-22 21:37:50 -0300

    3000

--- config/puma.rb
+++ config/puma.rb
@@ -32,7 +32,7 @@ end
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3001 }
+port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 environment rails_env
@@ -43,4 +43,4 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
-bind "tcp://0.0.0.0:#{ENV.fetch('PORT') { 3001 }}"
+bind "tcp://0.0.0.0:#{ENV.fetch('PORT') { 3000 }}"
