f0eb93ff61d692b2a835f83b4c1273ac6319ac6f
Wellyson Lopes <wellyson.dev@gmail.com>
2024-10-31 00:48:03 -0300

    Adiciona Gem annotate, e roda ocomando para adicionar os comentários

--- app/models/crime_type.rb
+++ app/models/crime_type.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: crime_types
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class CrimeType < ApplicationRecord
   has_many :incidents
 end
