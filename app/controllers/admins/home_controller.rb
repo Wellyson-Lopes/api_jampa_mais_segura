cf3b16f2837d161a63d3485e38add0e153c32b06
Wellyson Lopes <wellyson.dev@gmail.com>
2024-11-04 23:26:57 -0300

    Instala mapa leaflet

--- app/controllers/admins/home_controller.rb
+++ app/controllers/admins/home_controller.rb
@@ -1,5 +1,14 @@
 module Admins
   class HomeController < Admins::ApplicationAdminController
-    def index; end
+    def index
+      @ocorrencias = Incident.all
+      @dados = @ocorrencias.map do |ocorrencia|
+        {
+          lat: ocorrencia.latitude,
+          lng: ocorrencia.longitude,
+          popup: 'Informação opcional'
+        }
+      end
+    end
   end
 end
